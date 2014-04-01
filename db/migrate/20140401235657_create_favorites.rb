80c2397c6f5fb98406f3b69a1f1d1b2c8d15923d
Rahul Horé <hore.rahul@gmail.com>
2014-04-01 19:59:37 -0400

    Creates Favorite model and database table

---
+++ db/migrate/20140401235657_create_favorites.rb
@@ -0,0 +1,17 @@
+class CreateFavorites < ActiveRecord::Migration
+  def change
+    create_table :favorites do |t|
+      t.string :tweet_id
+      t.date :date_tweeted
+      t.text :text
+      t.string :tweeter_id
+      t.string :tweeter_name
+      t.string :tweeter_screen_name
+      t.integer :retweet_count
+      t.integer :favorite_count
+      t.text :urls
+
+      t.timestamps
+    end
+  end
+end
