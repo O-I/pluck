135b1b61efd876176c1c0e89f43adb127010fd6e
Rahul Horé <hore.rahul@gmail.com>
2014-04-02 15:06:57 -0400

    Initializes profile image url in RakeHelper

--- lib/tasks/rake_helper.rb
+++ lib/tasks/rake_helper.rb
@@ -1,15 +1,17 @@
 module RakeHelper
   def self.creator(fave)
-    Favorite.create( tweet_id: fave.id,
-                       date_tweeted: fave.created_at,
-                       text: fave.text,
-                       tweeter_id: fave.user[:id],
-                       tweeter_name: fave.user[:name],
-                       tweeter_screen_name: fave.user[:screen_name],
-                       retweet_count: fave.retweet_count,
-                       favorite_count: fave.favorite_count,
-                       urls: fave.attrs[:entities][:urls].map do |url|
-                               url[:display_url]
-                             end)
+    Favorite.create(   
+      tweet_id: fave.id,
+      date_tweeted: fave.created_at,
+      text: fave.text,
+      tweeter_id: fave.user[:id],
+      tweeter_name: fave.user[:name],
+      tweeter_screen_name: fave.user[:screen_name],
+      tweeter_profile_image_url: fave.user[:profile_image_url_https],
+      retweet_count: fave.retweet_count,
+      favorite_count: fave.favorite_count,
+      urls: fave.attrs[:entities][:urls].map do |url|
+              url[:display_url]
+            end)
   end
 end
