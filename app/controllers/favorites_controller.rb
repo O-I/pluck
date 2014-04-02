ed1cd5ad0d7592eac1c941f106c4257b4801058e
Rahul Horé <hore.rahul@gmail.com>
2014-04-01 23:31:19 -0400

    Implements favorites index and show actions

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,9 @@
 class FavoritesController < ApplicationController
   def index
+    @favorites = Favorite.page(params[:page]).order('created_at DESC')
   end
 
   def show
+    @favorite = Favorite.find(params[:id])
   end
 end
