0ae2df4f19875bc73816fa2aef1f346297477b7e
Rahul Horé <hore.rahul@gmail.com>
2014-04-03 16:39:53 -0400

    Restricts sort options to only the expected values

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,6 +10,15 @@ class FavoritesController < ApplicationController
   private
 
   def sort
-    params[:sort] || 'date_tweeted DESC'
+    "#{sort_column} #{sort_direction}"
+  end
+
+  def sort_column
+    sortable_columns = %w(date_tweeted favorite_count retweet_count)
+    sortable_columns.include?(params[:col]) ? params[:col] : 'date_tweeted'
+  end
+
+  def sort_direction
+    %w(ASC DESC).include?(params[:dir]) ? params[:dir] : 'DESC'
   end
 end
