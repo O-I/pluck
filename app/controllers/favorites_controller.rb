adb18acf25292698eac4e251e94b97c0c4b52e09
Rahul Horé <hore.rahul@gmail.com>
2014-04-03 16:14:05 -0400

    Adds private sort method to favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,15 @@
 class FavoritesController < ApplicationController
   def index
-    @favorites = Favorite.page(params[:page]).order(params[:sort])
+    @favorites = Favorite.page(params[:page]).order(sort)
   end
 
   def show
     @favorite = Favorite.find(params[:id])
+  end
+
+  private
+
+  def sort
+    params[:sort] || 'date_tweeted DESC'
   end
 end
