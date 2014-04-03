3581892772bf148a4a06d1033db9156657a49c23
Rahul Horé <hore.rahul@gmail.com>
2014-04-03 16:04:06 -0400

    Updates sortable method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def sortable(link_name, sort_by)
-    link_to link_name, sort: sort_by
+  def sortable(link_name, sort_column, sort_direction = 'DESC')
+    link_to link_name, sort: "#{sort_column} #{sort_direction}"
   end
 end
