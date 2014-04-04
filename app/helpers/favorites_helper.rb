1d38393f476f75007dab6aee43b2b23f1406ea2f
Rahul Horé <hore.rahul@gmail.com>
2014-04-04 14:59:26 -0400

    Adds link to the original tweet

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -2,4 +2,10 @@ module FavoritesHelper
   def link_to_tweeter(favorite)
     link_to favorite.tweeter_name, "https://twitter.com/#{favorite.tweeter_screen_name}", id: 'tweeter_link'
   end
+
+  def link_to_tweet_id(favorite)
+    link_to("#{time_ago_in_words favorite.date_tweeted} ago",
+            "https://twitter.com/#{favorite.tweeter_screen_name}/status/#{favorite.tweet_id}",
+            id: 'tweet_link')
+  end
 end
