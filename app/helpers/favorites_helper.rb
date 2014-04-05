dbbbd8e3940c06d880ba414f5854c3125c75aedd
Rahul Horé <hore.rahul@gmail.com>
2014-04-05 17:41:40 -0400

    Adds blank target to links

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,18 +1,18 @@
 module FavoritesHelper
   def link_to_tweeter(favorite)
-    link_to favorite.tweeter_name, "https://twitter.com/#{favorite.tweeter_screen_name}", id: 'tweeter_link'
+    link_to favorite.tweeter_name, "https://twitter.com/#{favorite.tweeter_screen_name}", id: 'tweeter_link', target: '_blank'
   end
 
   def link_to_tweet_id(favorite)
-    link_to("#{time_ago_in_words favorite.date_tweeted} ago",
+    link_to "#{time_ago_in_words favorite.date_tweeted} ago",
             "https://twitter.com/#{favorite.tweeter_screen_name}/status/#{favorite.tweet_id}",
-            id: 'tweet_link')
+            id: 'tweet_link', target: '_blank'
   end
 
   def text_to_true_link(tweet_text)
     urls = tweet_text.scan(/http\S*/)
     urls.each do |url|
-      tweet_text.gsub!(url, "<a href=#{url}>#{url}</a>")
+      tweet_text.gsub!(url, "<a href=#{url} target='_blank'>#{url}</a>")
     end
     tweet_text.html_safe
   end
