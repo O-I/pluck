67ba50dd234f018e775a5c4a1cce0df25977b690
Rahul Horé <hore.rahul@gmail.com>
2014-04-06 18:51:41 -0400

    Adds uniqueness validation on tweet_id

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,7 @@ class Favorite < ActiveRecord::Base
   serialize :urls, Array
   serialize :tweeter_profile_image_url, Addressable::URI
 
+  validates_uniqueness_of :tweet_id
+
   self.per_page = 25
 end
