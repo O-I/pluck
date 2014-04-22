9d47b0d46b9b8ae063d056529c8be4cbf11e39af
Rahul Horé <hore.rahul@gmail.com>
2014-04-22 16:59:24 -0400

    Adds basic respond_to tests to Favorite model

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Favorite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before { @fave = create(:favorite) }
+
+  subject { @fave }
+
+  it { should respond_to(:tweet_id) }
+  it { should respond_to(:date_tweeted) }
+  it { should respond_to(:text) }
+  it { should respond_to(:tweet_id) }
+  it { should respond_to(:tweeter_name) }
+  it { should respond_to(:tweeter_screen_name) }
+  it { should respond_to(:tweeter_profile_image_url) }
+  it { should respond_to(:retweet_count) }
+  it { should respond_to(:favorite_count) }
+  it { should respond_to(:urls) }
 end
