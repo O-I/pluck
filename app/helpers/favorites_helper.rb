58490c9b74e8355734857bb9d5a89bdd6a83cc0a
Rahul Horé <hore.rahul@gmail.com>
2014-04-23 19:24:31 -0400

    Converts tweet_link/tweeter_link from id to class

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,12 +1,12 @@
 module FavoritesHelper
   def link_to_tweeter(favorite)
-    link_to favorite.tweeter_name, "https://twitter.com/#{favorite.tweeter_screen_name}", id: 'tweeter_link', target: '_blank'
+    link_to favorite.tweeter_name, "https://twitter.com/#{favorite.tweeter_screen_name}", class: 'tweeter_link', target: '_blank'
   end
 
   def link_to_tweet_id(favorite)
     link_to "#{time_ago_in_words favorite.date_tweeted} ago",
             "https://twitter.com/#{favorite.tweeter_screen_name}/status/#{favorite.tweet_id}",
-            id: 'tweet_link', target: '_blank'
+            class: 'tweet_link', target: '_blank'
   end
 
   def text_to_true_link(tweet_text)
